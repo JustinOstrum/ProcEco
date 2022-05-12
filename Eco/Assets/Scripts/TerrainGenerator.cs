c83ebc5d8e36e7fffcc184f6fbd38b095a8b2b25
JustinOstrum <justin.ostrum@gmail.com>
2022-05-12 15:01:09 -0400

    Colourmap Added
    Completed the Colourmap portion. Now can display noisemap and colourmap

--- Eco/Assets/Scripts/TerrainGenerator.cs
+++ Eco/Assets/Scripts/TerrainGenerator.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
 {
+    public enum DrawMode {NoiseMap, ColourMap};
+    public DrawMode drawMode;
+
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
 
     public int octaves;
 
-    [Range(0,1)]
+    [Range(0, 1)]
     public float persistence;
     public float lacunarity;
 
@@ -22,34 +25,57 @@ public class TerrainGenerator : MonoBehaviour
     public TerrainType[] regions;
 
     public TerrainDisplay terrainDisplay;
-    
+
     public void GenerateMap()
     {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistence, lacunarity, offset);
 
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if (currentHeight <= regions[i].height)
+                    {
+                        colourMap[y * mapWidth + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
 
+        if(drawMode == DrawMode.NoiseMap)
+        {
+            terrainDisplay.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        }
 
-        terrainDisplay.DrawNoiseMap(noiseMap);
+        else if (drawMode == DrawMode.ColourMap)
+        {
+            terrainDisplay.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth,mapHeight));
+        }
     }
 
     private void OnValidate()
     {
-        if(mapWidth < 1)
+        if (mapWidth < 1)
         {
             mapWidth = 1;
         }
 
-        if(mapHeight < 1)
+        if (mapHeight < 1)
         {
             mapHeight = 1;
         }
 
-        if(lacunarity < 1)
+        if (lacunarity < 1)
         {
             lacunarity = 1;
         }
 
-        if(octaves < 0)
+        if (octaves < 0)
         {
             octaves = 0;
         }
