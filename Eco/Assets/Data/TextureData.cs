e5cba532e10bc48a1f5569ea394c37eeebbfba40
JustinOstrum <justin.ostrum@gmail.com>
2022-05-20 15:58:39 -0400

    Shader Difficulty
    Since the project is in the URP, there is continued issue with getting the shader working exactly how I want it.

--- Eco/Assets/Data/TextureData.cs
+++ Eco/Assets/Data/TextureData.cs
@@ -5,21 +5,11 @@ using UnityEngine;
 [CreateAssetMenu]
 public class TextureData : UpdatableData
 {
-    public Color[] baseColours;
-    [Range(0, 1)]
-    public float[] baseStartHeights;
-
-    public float[] baseBlends;
-
     float savedMinHeight;
     float savedMaxHeight;
 
     public void ApplyToMaterial(Material material)
     {
-        material.SetInt("_baseColourCount", baseColours.Length);
-        material.SetFloatArray("_baseBlends", baseBlends);
-        material.SetFloatArray("baseStartHeights", baseStartHeights);
-
         UpdateMeshHeights(material, savedMinHeight, savedMaxHeight);
     }
 
--- Eco/Assets/Data/UpdatableData.cs
+++ Eco/Assets/Data/UpdatableData.cs
@@ -24,4 +24,5 @@ public class UpdatableData : ScriptableObject
             OnValuesUpdated();
         }
     }
+    
 }
